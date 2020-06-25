67ce7c775509fe5bac7c36763cb7d8af015a1ba2
monir-007 <monir.hossain@northsouth.edu>
2020-06-26 00:52:32 +0600

    setup complete

--- src/App.js
+++ src/App.js
@@ -1,15 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        h1
-      </header>
-    </div>
-  );
+  return <> Hello from App</>;
 }
 
 export default App;
