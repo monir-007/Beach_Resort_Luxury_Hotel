4a8fa27ae9725946c8a6a59c5c68911ac70a402d
monir-007 <monir.hossain@northsouth.edu>
2020-07-01 01:54:02 +0600

    single room complete

--- src/context.js
+++ src/context.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import items from "./data";
 
 const RoomContext = React.createContext();
-// <RoomContext.Provider value={}
+// <RoomContext.Provider value={"hello"}
 export default class RoomProvider extends Component {
   state = {
     rooms: [],
@@ -16,7 +16,7 @@ export default class RoomProvider extends Component {
   componentDidMount() {
     //this.getData
     let rooms = this.formatData(items);
-    let featuredRooms = rooms.filter((room) => room.featuredRooms === true);
+    let featuredRooms = rooms.filter((room) => room.featured === true);
     this.setState({
       rooms,
       featuredRooms,
@@ -28,16 +28,22 @@ export default class RoomProvider extends Component {
   formatData(items) {
     let tempItems = items.map((item) => {
       let id = item.sys.id;
-      let images = item.fields.images.map((image) => image.fileds.file.url);
-      let room = { ...item.fileds, images, id };
+      let images = item.fields.images.map((image) => image.fields.file.url);
+      let room = { ...item.fields, images, id };
       return room;
     });
     return tempItems;
   }
 
+  getRoom = (slug) => {
+    let tempRooms = [...this.state.rooms];
+    const room = tempRooms.find((room) => room.slug === slug);
+    return room;
+  };
+
   render() {
     return (
-      <RoomContext.Provider value={{ ...this.state }}>
+      <RoomContext.Provider value={{ ...this.state, getRoom:this.getRoom }}>
         {this.props.children}
       </RoomContext.Provider>
     );
